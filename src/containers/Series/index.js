629663e184ad9d665dbe62def57407e7fa8cf5aa
Cata001 <catalinateguiani@yahoo.com>
2019-05-11 19:47:39 +0100

    Adding Form

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -6,21 +6,19 @@ class Series extends Component {
     series: []
   }
 
-  componentDidMount() {
-    // const series = ["Vikings", "Game of Thrones"];
-
-    // setTimeout(() => {
-    //   this.setState({series});
-    // }, 2000);
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
+  onSeriesInputChange = e => {
+    fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
       .then(response => response.json())
       .then(json => this.setState({series: json}))
-  }
+    }
 
   render() {
     return (
       <div>
         The length of series array - {this.state.series.length}
+        <div>
+          <input type="text" onChange={this.onSeriesInputChange}/>
+        </div>
         <SeriesList list={this.state.series} />
       </div>
     );
