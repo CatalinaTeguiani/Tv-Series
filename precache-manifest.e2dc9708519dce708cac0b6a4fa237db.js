21746c89daed4c079b06913ab49713f196bf564c
Cata001 <catalinateguiani@yahoo.com>
2019-07-17 22:17:36 +0100

    Updates

---
+++ precache-manifest.e2dc9708519dce708cac0b6a4fa237db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c354b7100952bc25c42d62749ff4a1a4",
+    "url": "/Tv-Series/index.html"
+  },
+  {
+    "revision": "80252875341cb8b8d2bd",
+    "url": "/Tv-Series/static/css/main.b8a7e025.chunk.css"
+  },
+  {
+    "revision": "e6c1f5ed117acbd14ccf",
+    "url": "/Tv-Series/static/js/2.f7d0a267.chunk.js"
+  },
+  {
+    "revision": "80252875341cb8b8d2bd",
+    "url": "/Tv-Series/static/js/main.4f4399e4.chunk.js"
+  },
+  {
+    "revision": "16c9b567046640a2c9a5",
+    "url": "/Tv-Series/static/js/runtime~main.1f6d8c3a.js"
+  }
+]);
